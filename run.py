4edb765e7c1159e66adb14ca4f2bf23c5355d1f7
Liviu Ene <eneliviu@gmail.com>
2024-05-27 17:50:22 +0200

    Fix registration, adding new tasks and user info updates

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 
+#%%
 #=================================================================#
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 #=================================================================#
@@ -31,7 +32,7 @@ USERS = 'users'
 TASKS = 'tasks'
 STATIC_OPTIONS = ['y', 'n']
 USER_HEADER = ['user_id', 'user_name', 'email', 'password', 'tasks']
-TASK_HEADER = ['user_id', 'task_id', 'description', 'category', 'created', 'due', 'status']
+TASK_HEADER = ['task_id', 'description', 'category', 'created', 'due', 'status']
 TASK_CATEGORY = ['errand', 'personal', 'work']
 
 # @dataclass
@@ -52,11 +53,10 @@ New users:
         - non-empty
         
 - input password
-
 '''
 
 #----------------------------------------------------------------#
-
+#%%
 def clean_cli() -> None:
 # https://www.geeksforgeeks.org/clear-screen-python/
     if os.name == 'nt':
@@ -75,12 +75,12 @@ def make_dict_from_nested_lists(list_data:list[list], d_keys:list[str]) -> dict:
     many regsitered users)        
     '''
 
-    #task_info.insert(0,task_header[1:])
-    #d_keys = list_data[0]
-
-    list_data.copy().insert(0, d_keys) # shallow list copy to avoid changing the original variable 
+    #list_data.copy().insert(0, d_keys) # shallow list copy to avoid changing the original variable 
 
     return {d_keys[i]:[s[i] for s in list_data] for i in range(len(d_keys)) }  
+
+#----------------------------------------------------------------#
+#%%
 
 def validate_password_length(user_password:str):
     '''
@@ -136,7 +136,6 @@ def validate_user_password_numerals(user_password:str):
     except ValueError as e:
         print(f'Invalid password: {e}, please try again.\n') 
         return False
-
 
 def validate_user_password(**kwargs):
     '''
@@ -213,9 +212,6 @@ def validate_user_email(user_email_column:list[str]) -> bool:
 
     return new_user_email
 
-
-
-
 def create_new_user_worksheet(new_user_data:dict) -> None:
     '''
     Validates username and password for new users by matching them against the
@@ -237,8 +233,6 @@ def create_new_user_worksheet(new_user_data:dict) -> None:
     SHEET.worksheet(new_user_data['user_name']).append_row(TASK_HEADER)
     SHEET.worksheet(USERS).append_row(list(new_user_data.values()))
 
-  
-
 
 def new_user_registration():
     '''
@@ -249,8 +243,8 @@ def new_user_registration():
         - values: column data (without header)
     ''' 
 
-    username_column = get_column(SHEET.worksheet(USERS), 'user_name', USER_HEADER)
-    user_email_column = get_column(SHEET.worksheet(USERS), 'email', USER_HEADER)
+    username_column, _ = get_column(SHEET.worksheet(USERS), 'user_name', USER_HEADER)
+    user_email_column, _ = get_column(SHEET.worksheet(USERS), 'email', USER_HEADER)
     
     new_user_name = validate_username(username_column)
     new_user_password = validate_user_password(length=8,
@@ -258,7 +252,7 @@ def new_user_registration():
                                             digits=2)
     new_user_email = validate_user_email(user_email_column)
 
-    new_user_data = [len(username_column) + 1, 
+    new_user_data = [len(username_column), 
                         new_user_name,
                         new_user_email,
                         new_user_password,
@@ -272,6 +266,7 @@ def new_user_registration():
             
     return new_user_data
 
+#%%
 #=================================================================#
 # ------------------- For Registered Users: ----------------------#
 
@@ -318,23 +313,23 @@ def match_user_credentials(user_data:dict, user_name:str, user_password:str) -> 
     Validates existing username and passowrd by matching them against the
     user records in the 'users' sheet. Returns a bool (True/False)  
     '''
-    while True:
-        if user_name == user_data['user_name'] and \
-            user_password == user_data['password']:   
-            return True
-        elif user_name != user_data['user_name'] and \
-                user_password != user_data['password']:
-            print('Username and password not found, please try again')
-            return False
-        elif user_name != user_data['user_name']:
-            print('Username not found, please try again')
-            return False
-        elif user_password != user_data['password']: 
-            print('Password not found, please try again')   
-            False
-        else:
-            return False
-
+    
+    if user_name == user_data['user_name'] and \
+        user_password == user_data['password']:   
+        return True
+    elif user_name != user_data['user_name'] and \
+            user_password != user_data['password']:
+        print('Username and password not found, please try again')
+        return False
+    elif user_name != user_data['user_name']:
+        print('Username not found, please try again')
+        return False
+    elif user_password != user_data['password']: 
+        print('Password not found, please try again')   
+        False
+    else:
+        return False
+    
 def get_sheet_meta(worksheet_name:str) -> list:
     wksheet = SHEET.worksheet(worksheet_name)
     wksheet_header = wksheet.row_values(1)
@@ -351,16 +346,16 @@ def get_column(worksheet:gspread.worksheet.Worksheet, column_name:str, header:li
     col_idx = header.index(column_name) + 1
     # Get the data in the 'user_id' column
     col_data = worksheet.col_values(col_idx)
-    return col_data
+    return col_data, col_idx
 
-def get_row_index(worksheet:gspread.worksheet.Worksheet, users_col:list[str], user_name:str) -> list:
+def get_row(worksheet:gspread.worksheet.Worksheet, users_col:list[str], user_name:str) -> list:
      # Get the index of the username in the column
     user_idx = users_col.index(user_name)+1
     # Retrieve the row containig the user info based n the username index:
     user_info = worksheet.row_values(user_idx)
-    return user_info
+    return user_info, user_idx
 
-def get_user_info(sheet_name:str, user_name:str, column_name:str) -> dict:
+def get_user_info(worksheet_name:str, user_name:str) -> dict:
     '''
     Get the user info from the 'users' worksheet based on 
     the username index. Can be handy when there are many 
@@ -371,37 +366,34 @@ def get_user_info(sheet_name:str, user_name:str, column_name:str) -> dict:
     '''
     
     # Connect to the worksheet and retrive the header:
-    users, user_header = get_sheet_meta(sheet_name)
+    #users, user_header = get_sheet_meta(sheet_name)
+    wksheet = SHEET.worksheet(worksheet_name)
 
-    # Get the user index in the user_id column:
-    #userame_col_idx = user_header.index(column_name) + 1
-    
-    # Get the usernames column from the worksheet indexed by userame_col_idx:
-    #users_col = users.col_values(userame_col_idx)
-    users_col = get_column(users, column_name, user_header)
+    # Get the 'user_name' column from the 'users'-worksheet:
+    users_col, _ = get_column(wksheet, 'user_name', USER_HEADER)
 
-    # Retrieve the row containig the user info based n the username index:
-    user_info = get_row_index(users, users_col, user_name)
+    # Retrieve the row containig the user information from 'users'-worksheet:
+    user_info, _ = get_row(wksheet, users_col, user_name)
 
-    return dict(zip(user_header, user_info))
+    return dict(zip(USER_HEADER, user_info))
 
-def list_tasks_simple(user_data:dict) -> tuple[list[list[str]], list[str]]:
+def tasks_list(user_data:dict) -> tuple[list[list[str]], list[str]]:
     '''
     Get the tasks by the user_id of a registered user.
     Returns the nested list of tasks (first return) and
     the header of the 'tasks' worksheet (second return).  
     '''
 
-    tasks, task_header = get_sheet_meta(user_data['user_name'])
-    # Get the data in the 'user_id' column
-    userid_col = get_column(tasks, 'user_id', task_header)
+    #tasks, task_header = get_sheet_meta(user_data['user_name'])
+    wksheet = SHEET.worksheet(user_data['user_name'])
+    # Get the data in the 'task_id' column
+    #column_data, _ = get_column(wksheet, column_name, TASK_HEADER)
 
     # Get all the row values for the tasks associated to an user_id:   
-    tasks_idx = [i+1 for i in range(len(userid_col)) if userid_col[i] == user_data['user_id']]
-    task_info = [tasks.row_values(int(t_ixd))[1:] for t_ixd in tasks_idx]
-    #make_dict_from_nested_lists(task_info, task_header[1:])
-
-    return tasks, task_info, task_header, tasks_idx
+    
+    task_info = make_dict_from_nested_lists(wksheet.get_all_values()[1:], TASK_HEADER)
+    
+    return wksheet, task_info
 
 def user_login() -> list:
     '''
@@ -412,8 +404,8 @@ def user_login() -> list:
         - values: column data (without header)
     '''
     while True:
-        user_input = input('Please enter your username and password separated by comma: ')
         
+        user_input = input('Please enter your username and password separated by comma: ')
         # Validate user_input: two strings separated by comma:
         user_input = validate_login_input(user_input)
         if user_input:
@@ -423,7 +415,7 @@ def user_login() -> list:
 
             # Get the user data from the 'users' Google worksheet:
             try:
-                user_data = get_user_info(USERS, user_name, 'user_name')
+                user_data = get_user_info(USERS, user_name)
                 if match_user_credentials(user_data, user_name, user_password):
                     print('Login successful!')
                     break
@@ -453,12 +445,7 @@ def validate_static_options(remove_choice:str, options:list[str]) -> bool:
             
     return True
     
-def delete_task(user_data:dict,
-                #tasks:gspread.worksheet.Worksheet,
-                #task_info:list[list[str]],
-                #task_header:list[str]
-                #tasks_idx:list[int]
-                **kwargs) -> None:
+def delete_task(user_data:dict, **kwargs) -> None:
     '''
     Delete one or more selected tasks by task id.
     The function removes the rows corresponding to the selected tasks from the 'tasks'Google worksheet.
@@ -480,7 +467,7 @@ def delete_task(user_data:dict,
     if validate_static_options(remove_choice, STATIC_OPTIONS):
         if remove_choice.lower() == 'y':
             task_remove_idx = sorted(list(set([int(k) for k in task_remove_idx.split(',')])))
-            user_task_data = make_dict_from_nested_lists(kwargs['task_info'], kwargs['task_header'][1:])
+            user_task_data = make_dict_from_nested_lists(kwargs['task_info'], kwargs['task_header'][0:])
             user_task_data['task_id'] = [int(i) for i in  user_task_data['task_id'] ]
 
             # Subset the worksheet rows to be deleted: 
@@ -498,9 +485,8 @@ def delete_task(user_data:dict,
             
             ## Update task index: 
             ### Get the row and column ids for the user: 
-            userid_col = get_column(kwargs['tasks'], 'user_id', kwargs['task_header'])   
+            userid_col, _ = get_column(kwargs['tasks'], 'user_id', kwargs['task_header'])   
             user_row_idx = [i+1  for i in range(len(userid_col)) if userid_col[i] == user_data['user_id']] 
-            #taskid_col = get_column(kwargs['tasks'], 'task_id', kwargs['task_header'])
             taskid_col = list(user_task_data.keys()).index('task_id') + 2
         
             update_idx = 1
@@ -561,51 +547,58 @@ def validate_due_date() -> datetime:
     '''
     Checks if the datetime input provides the valid day, month and year values in the 
     requested format MM-DD-YYY (e.g., 15-05-2024), otherwise it throws a ValueError.
+    The due date also has to be past the current time.
     '''
-    #due_date = due_date.split('-')
-    #due_date = [d.lstrip('0') for d in due_date]
 
     while True:
-        due_date = input('Enter the due date for the task in the MM-DD-YYYY format: ')
+        due_date = input('Enter the due date in the MM-DD-YYYY format (e.g., 1-12-2024): ')
+        creation_date = datetime.now()
         try:
             due_date = datetime.strptime(due_date, "%m-%d-%Y")
-            #doy = due_date[0]
-            #moy = due_date[1]
-            #yr  = due_date[2]
-            break
+            try:
+                if due_date < creation_date:
+                    raise ValueError('The date is later than the current time')
+                break
+            except ValueError as e:
+                print(f'Invalid time. {e}.')
         except ValueError as e:
-            print('Invalid format. Please enter the date as MM-DD-YYYY.')
-            
-    return due_date
+            print('Please enter a date in MM-DD-YYYY format.')
 
-def create_new_task() -> dict:
+    return due_date.strftime("%m-%d-%Y"), creation_date.strftime("%m-%d-%Y")
+
+def add_task(user_data:str) -> None:
     '''
     Add a new task to the user worksheet. The new task is inserted such that 
     it maintains the default sorting by due date, with the earliest due date at the top. 
     '''
-        
-    new_task = validate_new_task_description()
-    print('New task added.')
-    task_category = validate_new_task_category()
-    print(f'Task category {task_category.upper()} added.')
-    due_date = validate_due_date()
-    print(f'Task due date {due_date} added.')   
-    task_info = [new_task, task_category,'chore', due_date, 'active']
 
-    #user_id
-    #task_id 
+    #_, task_row = tasks_list(user_data)
+    user_data = get_user_info(USERS, user_data['user_name'])
 
-    # SHEET.add_worksheet(title = kwargs['user_name'], rows=1000, cols = len(TASK_HEADER))
-    # # Write the default worksheet column names:
-    # SHEET.worksheet(kwargs['user_name']).append_row(TASK_HEADER)
-    # new_user_data = [len(username_column) + 1, 
-    #                 kwargs['user_name'],
-    #                 kwargs['user_email'],
-    #                 kwargs['password'],
-    #                 kwargs['tasks']]
-    # SHEET.worksheet(USERS).append_row(new_user_data)
+    task_row = dict.fromkeys(TASK_HEADER)
+    task_row['description'] = validate_new_task_description()
+    task_row['category'] = validate_new_task_category() 
+    task_row['due'], task_row['created'] = validate_due_date()
+    task_row['status'] = 'active'
+    task_row['task_id'] = str(int(user_data['tasks']) + 1)
+    task_info = list(task_row.values())
+    #print(tabulate([task_info], headers=TASK_HEADER))
 
+        #print(tabulate(task_row, headers="keys", numalign="center"))
 
+    SHEET.worksheet(user_data['user_name']).append_row(task_info)
+    # Update the  number of tasks in the 'tasks' column from the 'users'-worksheet:
+
+    user_column, _ = get_column(SHEET.worksheet(USERS), 'user_name', USER_HEADER)
+    user_row, row_index = get_row(SHEET.worksheet(USERS), user_column, user_data['user_name'])
+    user_row = dict(zip(USER_HEADER, user_row))
+    user_row['tasks'] = str(int(user_row['tasks']) + 1)
+
+    # Update user row:
+    _, column_index = get_column(SHEET.worksheet(USERS), 'tasks', USER_HEADER)
+    SHEET.worksheet(USERS).update_cell(row_index, column_index, int(user_row['tasks']))
+    #SHEET.worksheet(USERS).insert_row(list(user_row.values()),
+    #                                        int(user_row['user_id']) + 1)
 
     return dict(zip(TASK_HEADER, task_info))
 
@@ -619,10 +612,10 @@ def main_menu() -> None:
     #print('Select an option: 1(Login), 2(Register), 3(Remove user), 4(Help), 5(Exit):')
 
     while True:
-        input_option = int(input('Select an option: 1(Login), 2(Register), 3(Remove user), 4(Help), 5(Exit): '))
-        if(input_option not in range(1, 5)):
+        input_option = int(input('Select: 1 (Login), 2 (Register), 3 (Delete account), 4 (Help), 5 (Exit): '))
+        if(input_option not in range(1, 6)):
             print('Invalid choice!\n'
-                  'Enter a valid option: 1(Login), 2(Register), 3(Remove user), 4(Help), 5(Exit):')
+                'Valid options: 1(Login), 2(Register), 3(Remove user), 4(Help), 5(Exit):')
         else:
             break
     return input_option
@@ -660,40 +653,47 @@ def task_handler(user_data:dict) -> None:
                 'Press Escape(Esc) to Cancel.')
         else:
             if user_choice != 4:
-                tasks, task_info, task_header, tasks_idx = list_tasks_simple(user_data)
+                tasks, task_info = tasks_list(user_data)
                 #print('User tasks retrieved.\n')
             else:
                 break # Exit the outer if-else
 
             if user_choice == 1: # View tasks
+
+                print(user_data)
+                tasks, task_info = tasks_list(user_data)
+                
                 if len(task_info) == 0:
                         print('You have no scheduled tasks.')
+                        
                 else:
                     print('Your tasks are listed below:')
                     # Formatted user tasks console print
-                    print(tabulate(task_info, headers = task_header[1:]))
+                    print(tabulate(list(task_info.values()), headers = TASK_HEADER))
                     clean_cli = input('Press y(Yes) to clear the output, or n(No) otherwise: ')
                     clear_output(clean_cli)
                     task_handler(user_data)
                 #break
-            elif user_choice == 2: # Add ask
-                create_new_task()
+            elif user_choice == 2: # Add a new ask
+                add_task(user_data)
                 task_handler(user_data)
             elif user_choice == 3: # Delete task
                 print('Your tasks are listed below:')
                 # Formatted user tasks console print
-                print(tabulate(task_info, headers = task_header[1:]))
+                print(tabulate(task_info, headers = TASK_HEADER))
                 delete_task(user_data = user_data,
                             tasks = tasks,
-                            task_info = task_info,
-                            task_header = task_header,
+                            task_info = list(task_info.values()),
+                            task_header = TASK_HEADER,
                             tasks_idx = tasks_idx)
+                
                 clean_cli = input('Press y(Yes) to clear the output, or n(No) otherwise: ')
                 clear_output(clean_cli)
                 task_handler(user_data)  
             else:
-                return False
-                
+                break
+
+#%%                
 #=================================================================#
 # -------------- Run the App: ---------------------------#
 
